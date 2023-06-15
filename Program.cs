fbd5a6fa30abdb8c3909657cb685b760ea10b98c
Collin Swisher <collinswish1@gmail.com>
2023-06-15 18:15:08 -0500

    Inital commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace Audio_Bot
+{
+	internal class Program
+	{
+		static async Task Main(string[] args)
+		{
+
+		}
+	}
+}
